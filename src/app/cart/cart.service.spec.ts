130b38103257263fd7bf7e43eb4a99fc1f6001cc
Wahab Hussain <hellowahab@gmail.com>
2023-08-14 13:25:55 +0500

    Completed all 12 modules

--- src/app/cart/cart.service.spec.ts
+++ src/app/cart/cart.service.spec.ts
@@ -10,7 +10,14 @@ describe('CartService', () => {
     service = TestBed.inject(CartService);
   });
 
-  it('should be created', () => {
-    expect(service).toBeTruthy();
+  it('should have the returned data in cart', (done) => {
+    // let req = httpTestingController.expectOne('api/cart');
+    // req.flush(TESTDATA);
+
+    service.getCart().subscribe((data) => {
+      expect(data.length).toEqual(1);
+      done();
+    })
+    
   });
 });
